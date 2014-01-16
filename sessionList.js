82becf3213013ceac4682bb9a4bffc1130955698
Michael Heggeseth <mheggeseth@yahoo.com>
2014-01-15 22:03:08 -0600

    Partition sessions by date

--- sessionList.js
+++ sessionList.js
@@ -7,14 +7,60 @@ SessionList = function (s) {
 	var list = ko.observableArray(s);
 	list.onlyFavorites = ko.observable(false);
 	list.showOld = ko.observable(true);
-	list.selected = list.filter(function (session) { //use knockout-projections
+	list.days = ko.computed(function days() {
+		var days = ko.utils.arrayMap(list(),
+			function days$arrayMap(session)
+			{
+				var sessionDateTime = moment(session.ScheduledDateTime);
+				if (list.showOld() || moment(sessionDateTime).add("hours", 1).isAfter())
+				{
+					return sessionDateTime.startOf("day").valueOf();
+				}
+			});
+		days = ko.utils.arrayFilter(days, function (day) { return day; }); //drop any items that are null or undefined
+		var distinctDays = ko.utils.arrayGetDistinctValues(days);
+		distinctDays.sort();
+		distinctDays = ko.utils.arrayMap(distinctDays,
+			function (dayMs)
+			{
+				return { date: moment(dayMs), selected: ko.observable(false) };
+			});
+		//Ensure mutual exclusivity in selection
+		ko.utils.arrayForEach(distinctDays,
+			function(day)
+			{
+				day.selected.subscribe(function(value)
+				{
+					if (value)
+					{
+						for (var i = 0; i < distinctDays.length; i++) {
+							if (distinctDays[i] !== day)
+							{
+								distinctDays[i].selected(false);
+							}
+						};
+					}
+				});
+			});
+		if (distinctDays.length)
+		{
+			distinctDays[0].selected(true);
+		}
+		return distinctDays;
+	});
+	list.selectedDay = ko.computed(function selectedDay() 
+	{
+		return ko.utils.arrayFirst(list.days(), function(day) { return day.selected(); });
+	});
+	list.selected = list.filter(function selected(session) { //use knockout-projections
 		var include = true;
 		include = !list.onlyFavorites() || session.isFavorite();
 		if (!list.showOld()) {
 			var currentDate = moment();
 			currentDate.subtract("hours", 1); //Also list sessions that are currently happening
-			include = (session.ScheduledDateTime > currentDate);
+			include = include && (session.ScheduledDateTime > currentDate);
 		}
+		include = include && moment(session.ScheduledDateTime).startOf("day").isSame(list.selectedDay().date);
 		return include;
 	});
 	return list;
